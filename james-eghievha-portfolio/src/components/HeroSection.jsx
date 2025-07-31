f502f800445444e4e51cafe4a9ea7f392aa2e1e3
James Eghievha <jameseghievha@gmail.com>
2025-07-31 17:44:37 +0100

    mobile responsive update for heros section

--- james-eghievha-portfolio/src/components/HeroSection.jsx
+++ james-eghievha-portfolio/src/components/HeroSection.jsx
@@ -3,10 +3,10 @@ import { ArrowDown } from "lucide-react";
 
 export const HeroSection = () => {
     return <section id="hero" 
-    className= "relative min-h-screen flex flex-col items-center justify-center px-4"
+    className= "relative min-h-screen flex flex-col items-center justify-center px-4 pt-20 md:pt-32"
     >
         <div className= "container max-w-4xl mx-auto text-center z-10">
-            <div className= "space-y-6">
+            <div className= "space-y-6 ">
                 <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
                     <span className="opacity-0 animate-fade-in">Hi, I'm</span>
                     <span className="text-primary opacity-0 animate-fade-in-delay-1">
